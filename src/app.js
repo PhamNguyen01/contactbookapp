93de37a0173f0a1e5f26193a20e8bdd81345105d
nguyen <nguyenb1910674@student.ctu.edu.vn>
2022-09-17 08:11:30 +0700

    Imlement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
+//1
 const express = require('express');
 const cors = require ('cors');
-const contactController = require('./controllers/contact.controller');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to contact book application. ' });
+});
+
+module.exports = app;
+
+//2
+
+const contactController = require('./controllers/contact.controller');
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
@@ -23,12 +35,36 @@ app.route('/api/contacts/:id')
 
 module.exports = app;
 
+//3
 
-app.use(cors());
-app.use(express.json());
+const ApiError = require('./api-error');
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application. ' });
+
+
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+// Handle 404 response.
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+// Define error-handling middleware last, after other app.use() and routes calls.
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
 });
 
-module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
